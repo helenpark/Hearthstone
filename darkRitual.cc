4f1aa633f87f12a417a6395f1b18b00ac7f86022
Stanley Szeto <sszeto@natural-flavours.csclub.uwaterloo.ca>
2017-07-24 19:01:44 -0400

    outline of the ritual cards

--- darkRitual.cc
+++ darkRitual.cc
@@ -13,11 +13,12 @@ DarkRitual:: DarkRitual(int p):Ritual{name2, cost2,
                 description2, p, activationCost2, charges2}{}
 
 
-/*
+
 DarkRitual::void useAbility(Target *t=nullptr){
     if(canActivate()){
-        o->gainMP();
+	// t should be a pointer to a player, so that it gains MP
+        t->gainMP();
         charges -= activationCost;
     }
 }
-*/
+
